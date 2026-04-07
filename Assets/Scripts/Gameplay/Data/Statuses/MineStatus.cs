e3ae85f7cf46daac31b1fcbfb12c5cd7f8f60bd9
nyarla <nyarladev@gmail.com>
2026-04-07 09:40:21 +0300

    Mine unit

---
+++ Assets/Scripts/Gameplay/Data/Statuses/MineStatus.cs
@@ -0,0 +1,59 @@
+﻿using System.Linq;
+using Gameplay.Data.Effects;
+using Gameplay.Data.Validator;
+using Gameplay.Units;
+using UnityEngine;
+using Zenject;
+
+namespace Gameplay.Data.Statuses
+{
+    [CreateAssetMenu(menuName = "Gameplay Data/Statuses/Mine", order = 0)]
+    public class MineStatus : StatusType
+    {
+        [SerializeField] private SOInjectPresenter _gameplayPresenter;
+        [SerializeField] private float _triggerRadius;
+        [SerializeField] private int _cooldown;
+        [SerializeField] private UnitValidatorGroup _triggerValidator;
+        [SerializeField] private EffectTargetingUnit[] _triggerEffects;
+        [SerializeField] private EffectTargetingUnit[] _mineTriggerEffects;
+        
+        [Inject] private IsometricOverlap Overlap { get; } 
+        
+        public override void OnAdd(Status status)
+        {
+            _gameplayPresenter.Inject(this);
+        }
+
+        public override void OnUpdate(Status status)
+        {
+            if (status.CurrentFrame % 5 != 0)
+                return;
+            if ( ! status.Host.Simulation.IsSimulated)
+                return;
+            if (status.IsLocked)
+                return;
+
+            Overlap.TryGetUnits(status.Host.Position, _triggerRadius, out Unit[] triggeringUnits);
+            Unit triggeringUnit = triggeringUnits.FirstOrDefault(u => _triggerValidator.IsValid(status.Host, u));
+            if ( ! triggeringUnit)
+                return;
+            
+            foreach (EffectTargetingUnit effect in _triggerEffects)
+            {
+                effect.Apply(status.Host, triggeringUnits[0]);
+            }
+
+            foreach (EffectTargetingUnit effect in _mineTriggerEffects)
+            {
+                effect.Apply(status.Host, status.Host);
+            }
+            
+            status.Host.Kill();
+        }
+
+        public override void OnRemove(Status status)
+        {
+            
+        }
+    }
+}
