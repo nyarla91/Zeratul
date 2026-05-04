358d4e10264279461a09852fd51f0a37712bc7d7
nyarla <nyarladev@gmail.com>
2026-05-04 14:28:43 +0300

    Fog of war saving

---
+++ Assets/Scripts/Gameplay/Saving/MapSavingSystem.cs
@@ -0,0 +1,29 @@
+﻿using Extentions;
+using Gameplay.Vision;
+using Saving.Data;
+using UnityEngine;
+
+namespace Gameplay.Saving
+{
+    public class MapSavingSystem : SavingSystem<MapSaveSystem>
+    {
+        [SerializeField] private Camera _camera;
+        [SerializeField] private FogOfWar _fogOfWar;
+        
+        protected override string LoadKey => MapSaveSystem.LoadKey;
+        
+        public override void ReproduceFromSaveData(MapSaveSystem payload)
+        {
+            _camera.transform.position = payload.cameraPosition.ToVector2().WithZ(_camera.transform.position.z);
+            _camera.orthographicSize = payload.cameraSize;
+            _fogOfWar.ReproduceFromSaveData(payload);
+        }
+
+        public override ISaveSystem Save()
+        {
+            SerializableVector2 cameraPosition = SerializableVector2.FromVector2(_camera.transform.position);
+            bool[,] scoutedFogOfWar = _fogOfWar.GetScouted();
+            return new MapSaveSystem(cameraPosition, _camera.orthographicSize, scoutedFogOfWar);
+        }
+    }
+}
--- Assets/Scripts/Gameplay/Vision/FogOfWar.cs
+++ Assets/Scripts/Gameplay/Vision/FogOfWar.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Gameplay.Data.Configs;
+using Saving.Data;
 using UniRx;
 using UniRx.Triggers;
 using UnityEngine;
@@ -19,7 +20,9 @@ namespace Gameplay.Vision
         [SerializeField] private Color _revealedColor;
         [SerializeField] private LayerMask _revealMask;
 
-        private Sprite _targetSprite;
+        private Sprite TargetSprite => _spriteRenderer.sprite;
+
+        private bool _loaded;
 
         private void Start()
         {
@@ -28,16 +31,48 @@ namespace Gameplay.Vision
                 .Subscribe(_ => RecalculateFog());
             
             transform.localScale = _pixelScale * Vector3.one;
-            _targetSprite = _spriteRenderer.sprite;
+            
+            if (_loaded)
+                return;
             
             for (int y = 0; y < _fogDimensions.y; y++)
             {
                 for (int x = 0; x < _fogDimensions.x; x++)
                 {
-                    _targetSprite.texture.SetPixel(x, y, _hiddenColor);
+                    TargetSprite.texture.SetPixel(x, y, _hiddenColor);
                 }
             }
-            _targetSprite.texture.Apply();
+            TargetSprite.texture.Apply();
+        }
+
+        public void ReproduceFromSaveData(MapSaveSystem saveSystem)
+        {
+            bool[,] scouted = saveSystem.scoutedFogOfWar;
+            for (int y = 0; y < _fogDimensions.y && y < scouted.GetLength(1); y++)
+            {
+                for (int x = 0; x < _fogDimensions.x && x < scouted.GetLength(0); x++)
+                {
+                    Debug.Log($"{x} {y} {scouted[x, y]}");
+                    Color color = scouted[x, y] ? _scoutedColor : _hiddenColor;
+                    TargetSprite.texture.SetPixel(x, y, color);
+                }
+            }
+            TargetSprite.texture.Apply();
+            _loaded = true;
+        }
+
+        public bool[,] GetScouted()
+        {
+            bool[,] result = new bool[_fogDimensions.x, _fogDimensions.y];
+            
+            for (int y = 0; y < _fogDimensions.y; y++)
+            {
+                for (int x = 0; x < _fogDimensions.x; x++)
+                {
+                    result[x, y] = ! TargetSprite.texture.GetPixel(x, y).Equals(_hiddenColor);
+                }
+            }
+            return result;
         }
 
         private void RecalculateFog()
@@ -50,12 +85,12 @@ namespace Gameplay.Vision
                     bool revealed = Physics2D.OverlapPoint(point, _revealMask);
                     
                     if (revealed)
-                        _targetSprite.texture.SetPixel(x, y, _revealedColor);
-                    else if (_targetSprite.texture.GetPixel(x, y).Equals(_revealedColor))
-                        _targetSprite.texture.SetPixel(x, y, _scoutedColor);
+                        TargetSprite.texture.SetPixel(x, y, _revealedColor);
+                    else if (TargetSprite.texture.GetPixel(x, y).Equals(_revealedColor))
+                        TargetSprite.texture.SetPixel(x, y, _scoutedColor);
                 }
             }
-            _targetSprite.texture.Apply();
+            TargetSprite.texture.Apply();
         }
     }
 }
